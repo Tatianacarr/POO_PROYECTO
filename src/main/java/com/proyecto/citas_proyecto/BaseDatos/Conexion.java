26fee314877c58f1d43c079144bcdaca2bc81a7d
Angui_Fierro <anguifierro56@gmail.com>
2026-07-05 20:45:27 -0500

    Agrego conexion a PostgreSQL y avance del sistema

--- src/main/java/com/proyecto/citas_proyecto/BaseDatos/Conexion.java
+++ src/main/java/com/proyecto/citas_proyecto/BaseDatos/Conexion.java
@@ -1,4 +1,35 @@
 package com.proyecto.citas_proyecto.BaseDatos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexion {
+    private static final String url= "jdbc:postgresql://localhost:5432/Citas_medicas";
+    private static final String USER= "postgres";
+    private static final String PASSWORD="123456";
+
+    private static Connection conexion;
+    public static Connection getConexion(){
+        try{
+            if (conexion == null || conexion.isClosed()){
+                conexion = DriverManager.getConnection(url,USER,PASSWORD);
+                System.out.println("Conexion exitosa");
+
+            }
+        } catch (SQLException e) {
+            System.out.println("Error de conexion " + e.getMessage());
+        }
+        return conexion;
+    }
+    public static void cerrarConexion(){
+        try{
+            if (conexion !=null && !conexion.isClosed()){
+                conexion.close();
+                System.out.println("Conexion cerrada");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
