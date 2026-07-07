f708f11d65370d2282f2d779cad98c403811cd55
Angui_Fierro <anguifierro56@gmail.com>
2026-07-07 18:04:38 -0500

    Conexion PostgreSQL y login con registro de usuarios creados con fxml, los estilos y el LoginController y descarga de maven con las dependencias necesarias

--- src/main/java/com/proyecto/citas_proyecto/App/HelloApplication.java
+++ src/main/java/com/proyecto/citas_proyecto/App/HelloApplication.java
@@ -8,12 +8,23 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        FXMLLoader loader = new FXMLLoader(
+                HelloApplication.class.getResource("/com/proyecto/citas_proyecto/Login.fxml")
+        );
+
+        Scene scene = new Scene(loader.load(), 900, 550);
+
+        stage.setTitle("Sistema de Citas Médicas");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.proyecto.citas_proyecto {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
+    opens com.proyecto.citas_proyecto.Controller to javafx.fxml;
+    exports com.proyecto.citas_proyecto.Controller;
 
-    opens com.proyecto.citas_proyecto to javafx.fxml;
-    exports com.proyecto.citas_proyecto;
+    opens com.proyecto.citas_proyecto.App to javafx.fxml;
+    exports com.proyecto.citas_proyecto.App;
 }
