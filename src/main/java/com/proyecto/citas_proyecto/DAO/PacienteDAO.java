dac38b8e0b63f8c23b06b9a30b9858e88d047a63
Angui_Fierro <anguifierro56@gmail.com>
2026-07-08 20:30:05 -0500

    Se agregaron las interfaces DAO

--- src/main/java/com/proyecto/citas_proyecto/DAO/PacienteDAO.java
+++ src/main/java/com/proyecto/citas_proyecto/DAO/PacienteDAO.java
@@ -1,4 +1,17 @@
 package com.proyecto.citas_proyecto.DAO;
 
+import com.proyecto.citas_proyecto.Model.Paciente;
+import java.util.List;
+
 public interface PacienteDAO {
+
+    boolean guardar(Paciente paciente);
+
+    boolean actualizar(Paciente paciente);
+
+    boolean eliminar(int id);
+
+    Paciente buscarPorId(int id);
+
+    List<Paciente> listar();
 }
